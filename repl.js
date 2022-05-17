ff6926f1705e9e5b768d4295c59df939fb94f12d
martian17 <yakumodanshi@gmail.com>
2022-05-17 17:02:46 +0900

    ez repl hack

---
+++ repl.js
@@ -0,0 +1,29 @@
+let files = process.argv.slice(2);
+
+
+let repl = require("repl");
+
+const { Readable, Writable } = require('stream'); 
+
+const rs = new Readable({
+    read() {}
+});
+const ws = new Writable({
+	write() {}
+});
+let r2 = repl.start({
+    prompt: "",
+    input: rs,
+    output: ws,
+	useGlobal: true
+});
+for(let i = 0; i < files.length; i++){
+    r2.write(`.load ${files[i]}\n`);
+}
+
+let r = repl.start({
+	prompt:"> ",
+	useGlobal:true
+});
+
+
