c995d16e745e20689716c957926d8608c46210f5
martian17 <yakumodanshi@gmail.com>
2022-08-07 04:50:08 +0900

    replace tab with space

--- repl.js
+++ repl.js
@@ -9,21 +9,21 @@ const rs = new Readable({
     read() {}
 });
 const ws = new Writable({
-	write() {}
+    write() {}
 });
 let r2 = repl.start({
     prompt: "",
     input: rs,
     output: ws,
-	useGlobal: true
+    useGlobal: true
 });
 for(let i = 0; i < files.length; i++){
     r2.write(`.load ${files[i]}\n`);
 }
 
 let r = repl.start({
-	prompt:"> ",
-	useGlobal:true
+    prompt:"> ",
+    useGlobal:true
 });
 
 if("NODE_REPL_HISTORY" in process.env && process.env.NODE_REPL_HISTORY !== "")r.setupHistory(process.env.NODE_REPL_HISTORY,()=>{});
